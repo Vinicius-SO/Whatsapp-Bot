ed8fe0f6697ae8e106c6f33e6fe3910071876464
unknown <vhisoares39@gmail.com>
2024-10-21 15:05:45 -0300

    config only group messages

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const qrcode = require('qrcode-terminal');
-const { Client, NoAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const cron = require('node-cron');
 
 
 // equivalent to:
 const client = new Client({
-    authStrategy: new NoAuth()
+  authStrategy: new LocalAuth({
+    clientId: "bot-session",
+    dataPath: "./persist-data",  // Local onde os dados de sessão serão salvos
+  })
 });
-
 client.on('qr', (qr) => {
   console.log(qr)
     qrcode.generate(qr, { small: true });
 });
+
 
 client.on('ready', () => {
   console.log('Cliente pronto!');
@@ -46,56 +49,61 @@ client.on('ready', () => {
       }
   });
 });
-
 client.on('message_create', async (message) => {
 
-    if (message.body.startsWith('!btc')) {
-      const json = await fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCBRL')
-      const brutePrice = await json.json()
-      const formattedPrice = parseFloat(brutePrice.price).toFixed(2);
-      //com R$
-      const price = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      }).format(formattedPrice);
-
-      message.reply(`O valor do bitcoin está em ${price}`);
-      // const chatData = await message.getChat()
-      // console.log(chatData.groupChat.groupMetadata.id._serialized)
+  const chatData = await message.getChat()
+  if(chatData.groupMetadata){
+    if(chatData.groupMetadata.id._serialized == '120363332673166670@g.us'){
+      if (message.body.toLowerCase().startsWith('b')) {
+        const json = await fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCBRL')
+        const brutePrice = await json.json()
+        const formattedPrice = parseFloat(brutePrice.price).toFixed(2);
+        //com R$
+        const price = new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        }).format(formattedPrice);
+  
+        message.reply(`O valor do bitcoin está em ${price}`);
+       
+        // console.log(chatData.groupChat.groupMetadata.id._serialized)
+      }
+  
+      if (message.body.toLowerCase().startsWith('s')) { 
+        const json = await fetch('https://api.binance.com/api/v3/avgPrice?symbol=SOLBRL')
+        const brutePrice = await json.json()
+        const formattedPrice = parseFloat(brutePrice.price).toFixed(2);
+        const price = new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        }).format(formattedPrice);
+        message.reply(`O valor da solana está em ${price}`);
+        // const chatData = await message.getChat()
+        // console.log(chatData.groupChat.groupMetadata.id._serialized)
+      }
+      if (message.body.toLowerCase().startsWith('e')) {
+        const json = await fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHBRL')
+        const brutePrice = await json.json()
+        const formattedPrice = parseFloat(brutePrice.price).toFixed(2);
+        
+        const price = new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        }).format(formattedPrice);
+  
+        message.reply(`O valor do Ethereum está em ${price}`);
+        // const chatData = await message.getChat()
+        // console.log(chatData.groupChat.groupMetadata.id._serialized)
+      }
     }
-
-    if (message.body.toLowerCase().startsWith('!sol')) { 
-      const json = await fetch('https://api.binance.com/api/v3/avgPrice?symbol=SOLBRL')
-      const brutePrice = await json.json()
-      const formattedPrice = parseFloat(brutePrice.price).toFixed(2);
-      const price = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      }).format(formattedPrice);
-      message.reply(`O valor da solana está em ${price}`);
-      // const chatData = await message.getChat()
-      // console.log(chatData.groupChat.groupMetadata.id._serialized)
-    }
-    if (message.body.startsWith('!eth')) {
-      const json = await fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHBRL')
-      const brutePrice = await json.json()
-      const formattedPrice = parseFloat(brutePrice.price).toFixed(2);
-      
-      const price = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      }).format(formattedPrice);
-
-      message.reply(`O valor do Ethereum está em ${price}`);
-      // const chatData = await message.getChat()
-      // console.log(chatData.groupChat.groupMetadata.id._serialized)
-    }
+  }
+   
 });
 
 client.initialize();
